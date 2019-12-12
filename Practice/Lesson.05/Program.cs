e7efa4d41dfb074793e2e11d71a1bcf6c923c83f
Vasily-Shk <58305611+Vasily-Shk@users.noreply.github.com>
2019-12-13 01:23:26 +0300

    Кажется не уложился в 20 минут! )

---
+++ Practice/Lesson.05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Lesson._05
+{
+    class Program
+    {
+        static void Main()
+        {
+            TimeSpan CurTime = System.DateTime.Now.TimeOfDay;
+            int CurHours = CurTime.Hours;
+            string OutputConsoleString;
+            if (CurHours > 9 & CurHours < 12)
+            {
+                OutputConsoleString = "Good morning, guys!";
+            }
+            else if (CurHours >= 12 & CurHours < 15)
+            {
+                OutputConsoleString = "Good day, guys!";
+            }
+            else if (CurHours >= 15 & CurHours < 22)
+            {
+                OutputConsoleString = "Good evening, guys!";
+            }
+            else
+            {
+                OutputConsoleString = "Все лабораторку кодите? Удачи!";
+            }
+            Console.WriteLine(OutputConsoleString + "\nCurrent Time: " + CurTime.ToString());
+        }
+    }
+}
