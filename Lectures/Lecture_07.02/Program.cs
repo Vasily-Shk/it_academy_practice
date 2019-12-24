6043dc0e85b4b0524a3769d59f9482d3c997d335
Vasily-Shk <cad@tut.by>
2019-12-24 19:44:16 +0300

    Add 07_02

---
+++ Lectures/Lecture_07.02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lecture_07._02
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string testArr = new { 32, A,Hello};
+            for (int i = 0; i < 4; i++)
+            {
+                string s = testArr(i);
+                Console.WriteLine(s);
+            }
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
